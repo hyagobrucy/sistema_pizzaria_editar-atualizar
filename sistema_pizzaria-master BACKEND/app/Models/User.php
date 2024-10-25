4b5ee303b268c216634dd2626282bcbb7cbfb93c
Hyago Brucy <107419779+hyagobrucy@users.noreply.github.com>
2024-10-25 09:53:52 -0300

    Update User.php - SOLID

--- sistema_pizzaria-master BACKEND/app/Models/User.php
+++ sistema_pizzaria-master BACKEND/app/Models/User.php
@@ -2,60 +2,35 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-/**
- * Class User
- *
- * @package App\Models
- * @author Vinícius Siqueira
- * @link https://github.com/ViniciusSCS
- * @date 2024-10-01 15:50:56
- * @copyright UniEVANGÉLICA
- * @method static select(string $string, string $string1, string $string2)
- * @method static create(array $array)
- * @method static find(string $id)
- */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    // Adicione métodos específicos para manipulação de dados de usuário aqui
+    public function sendNotification($notification)
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        // lógica para enviar notificação
     }
 }
