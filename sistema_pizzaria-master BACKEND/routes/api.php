d8861a6a21d7c82795c9ea9d3c8d6aa1b3aeb2c6
Hyago Brucy <107419779+hyagobrucy@users.noreply.github.com>
2024-10-25 10:00:36 -0300

    Update api.php - SOLID

--- sistema_pizzaria-master BACKEND/routes/api.php
+++ sistema_pizzaria-master BACKEND/routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PizzaController;
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/users', [UserController::class, 'index']); // Listar usuários
-    Route::post('/users', [UserController::class, 'store']); // Criar usuário
-    Route::get('/users/{id}', [UserController::class, 'show']); // Visualizar usuário
-    Route::put('/users/{id}', [UserController::class, 'update']); // Atualizar usuário
-    Route::delete('/users/{id}', [UserController::class, 'destroy']); // Deletar usuário
+    Route::get('/users', [UserController::class, 'index']); 
+    Route::post('/users', [UserController::class, 'store']); 
+    Route::get('/users/{id}', [UserController::class, 'show']); 
+    Route::put('/users/{id}', [UserController::class, 'update']); 
+    Route::delete('/users/{id}', [UserController::class, 'destroy']); 
+
+    Route::get('/pizzas', [PizzaController::class, 'index']); 
+    Route::post('/pizzas', [PizzaController::class, 'store']); 
 });
